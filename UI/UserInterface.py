2b52025bb19f8b5f92dde950e98342cfaa9497a3
PATRICK REDMAN <predman356@googlemail.com>
2024-06-04 14:39:13 +0100

    Added the functionality for the user to select companies from the returned checkbox list and download them, selecting the local file location for the file to be saved in.

--- UI/UserInterface.py
+++ UI/UserInterface.py
@@ -5,7 +5,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-from Scraper.scraper import company_search_results
+from tkinter import filedialog
+import Scraper.scraper as scraper
 
 # Set up the main window
 root = Tk()
@@ -40,7 +41,7 @@ def search_action():
     if search_query == '' or search_query == 'Enter a company name here':
         messagebox.showwarning("Input Error", "Please enter a valid company name.")
     else:
-        companies = company_search_results(search_query)
+        companies = scraper.company_search_results(search_query)
         if companies:
             display_companies(companies)
         else:
@@ -60,7 +61,49 @@ def on_focus_out(event):
         search_entry.insert(0, 'Enter a company name here')
         search_entry.config(fg='grey')
 
-# Create and place the search entry using grid
+# Function to clear all checkboxes in the given frame
+def clear_checkboxes(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+
+
+# Display the companies for the user to select from
+checkbox_vars = []
+def display_companies(companies):
+
+    global checkbox_vars
+    checkbox_vars = []
+
+    clear_checkboxes(checkbox_frame)
+
+    for company in companies:
+        var = IntVar()
+        chk = Checkbutton(checkbox_frame, text=company[0], variable=var)
+        chk.pack(anchor='w')
+        checkbox_vars.append((var, company[0], company[1]))
+
+# Function to download the selected companies
+def company_PDFs(event=None):
+    selected_companies = [(var, name, link) for var, name, link in checkbox_vars if var.get() == 1]  # Extract only selected companies
+    file_location = ""
+    while not file_location:
+        file_location = save_file_dialog()
+    
+    if not selected_companies:
+        messagebox.showerror("Error", "Please select a company")
+    else:
+        print(selected_companies)
+        for var, name, link in selected_companies:
+            scraper.company_reports_download(link, name, file_location)
+
+# Function to get the user to select a filesave location
+def save_file_dialog():
+    file_location = filedialog.askdirectory(title="Select a folder")
+    return file_location    
+
+
+# Create and place the search entry, button and selection button using grid
 search_entry.insert(0, 'Enter a company name here')
 search_entry.bind('<FocusIn>', on_entry_click)
 search_entry.bind('<FocusOut>', on_focus_out)
@@ -68,25 +111,17 @@ search_entry.bind('<FocusOut>', on_focus_out)
 search_button = Button(root, text="Search", command=search_action)
 search_button.grid(row=1, column=1, padx=10, pady=10)
 
+selection_button = Button(root, text="Select companies")
+selection_button.grid(row=2, column=1, padx=10, pady=10)
+selection_button.bind("<Button-1>", lambda event: company_PDFs(event))
+
 #Create the frame for the checkboxes
-frame = Frame(root)
-frame.grid(row=2, column=0, padx=10, pady=10)
-
-# Function to clear all checkboxes in the given frame
-def clear_checkboxes(frame):
-    for widget in frame.winfo_children():
-        widget.destroy()
+checkbox_frame = Frame(root)
+checkbox_frame.grid(row=2, column=0, padx=10, pady=10)
 
 
-# Display the companies for the user to select from
-def display_companies(companies):
 
-    clear_checkboxes(frame)
 
-    for company in companies:
-        var = IntVar()
-        chk = Checkbutton(frame, text=company[0], variable=var)
-        chk.pack(anchor='w')
 
 
 # Start the Tkinter event loop
